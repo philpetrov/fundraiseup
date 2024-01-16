082683413c57020b0f1d7778161f9bd41eb2da2d
ppetrovsky <ppetrovsky@norwexic.com>
2024-01-16 19:31:32 +0100

    add elements

--- e2e/pages/mainPage.js
+++ e2e/pages/mainPage.js
@@ -6,18 +6,21 @@ exports.MainPage = class MainPage {
 
     this.page = page
 
-    this.giveNowBtn = page.frameLocator('iframe[title="Donate Button"]').getByRole('button', { name: 'Give Now' })
+    this.giveNowBtn = page.frameLocator('iframe[title="Donation Widget"]').getByRole('button', { name: 'Give Now' });
     this.monthlyBtn = page.frameLocator('iframe[title="Donation Widget"]').getByRole('button', { name: 'Monthly' })
     this.donateMonthlyBtn = page.frameLocator('iframe[title="Donation Widget"]').getByRole('button', { name: 'Donate monthly' })
-    this.donateMonthlyBtn = page.frameLocator('iframe[title="Donation Widget"]').getByRole('button', { name: 'Credit card' }).click();
+    this.creditCardBtn = page.frameLocator('iframe[title="Donation Widget"]').getByRole('button', { name: 'Credit card' })
 
     this.priceInput = page.frameLocator('iframe[title="Donation Widget"]').getByTestId('price-input');
+    this.cardNumberInput = page.frameLocator('iframe[title="Donation Widget"]').frameLocator('iframe[name="__privateStripeFrame6416"]').getByPlaceholder('Card number')
+    this.monthYearInput = page.frameLocator('iframe[title="Donation Widget"]').frameLocator('iframe[name="__privateStripeFrame6417"]').getByPlaceholder('MM / YY')
 
     this.coverTransactionCostCheckBox = page.frameLocator('iframe[title="Donation Widget"]').getByRole('checkbox', { name: 'Cover transaction costs' });
   }
 
   async goto() {
     await this.page.goto(config.baseUrl + '/qa-test-7R58U3/');
+    this.page.waitForLoadState('domcontentloaded');
   }
 
   async clickGiveNowBtn() {
@@ -36,10 +39,19 @@ exports.MainPage = class MainPage {
     if (isActive === true) {
         await checkBox.click();
     }
-}
+  }
+  async clickCreditCardBtn() {
+    await this.creditCardBtn.click();
+  }
+  async clickMonthYearInput() {
+    await this.monthYearInput.click();
+  }
 
   async typePriceInput(price) {
     await this.priceInput.fill(price);
+  }
+  async typeCardNumberInput(cardNumber) {
+    await this.cardNumberInput.fill(cardNumber);
   }
 
 }
