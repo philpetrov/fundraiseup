799965810560dff5b91282f7f43d1306795bc8c2
ppetrovsky <ppetrovsky@norwexic.com>
2024-01-19 05:16:50 +0100

    test project is ready

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# project: "test task using playwright"
+# Instructions for test project: "test task using playwright"
 
-# run codegen
-- npx playwright codegen
+# 1. Install "node": ">=16" locally, version: 20.4.0 recommended:
 
-# Start one specific test with npx chromium and headed
-- npx playwright test --project=chromium --headed --repeat-each=5
+# 2. Install dependecies from package.json using command:
 
-# Start in parralel:
-- npx jest --maxWorkers=2
+- npm install
+
+# To start tests (destop then mobile) with in headed mode in parallel (repeat=1) with saving index.html report in playwright-report folder run this command in terminal:
+
+- npx playwright test --project="Desktop Chrome" --headed --repeat-each=1 --workers=2 --reporter=html && npx playwright test --project="Mobile Chrome" --headed --repeat-each=1 --workers=2 --reporter=html
+
+# To show html report after running tests run this command:
+
+- npx playwright show-report
--- tests/e2e/specs/payments.spec.js
+++ tests/e2e/specs/payments.spec.js
@@ -1,31 +1,122 @@
-const { test } = require('@playwright/test');
-import { MainPage } from '../../../src/pages/main.page';
-import { invalidCreditCardData, testContactData } from '../../../config/test-data/payments-test-data';
+const { test } = require("@playwright/test");
+import { MainPage } from "../../../src/pages/main.page";
+import {
+  invalidCreditCardData,
+  testContactData,
+} from "../../../config/test-data/payments-test-data";
 
-test.describe('Payment scenarios', () => {
-    test('A1. Payment scenario with credit card failure', async ({page}) => {
+test.describe.parallel("Payment scenarios", () => {
+  test.beforeEach(async ({}, testInfo) => {
+    testInfo.setTimeout(120000);
+  });
 
-        const mainPage = new MainPage(page)
+  test("A1. Payment scenario with incorrect credit card", async ({ page }) => {
+    const mainPage = new MainPage(page);
 
-        await mainPage.goto()
-        await mainPage.clickGiveNowBtn()
-        await mainPage.selectFromCurrencyList('USD')
-        await mainPage.typePriceInput('0')
-        await mainPage.typePriceInput('100')
-        await mainPage.clickMonthlyBtn()
-        await mainPage.clickDonateMonthlyBtn()
-        await mainPage.clickCoverTransactionCostIfActiveCheckBox()
-        await mainPage.clickCreditCardBtn()
-        await mainPage.typeCardNumberInput(invalidCreditCardData.cardNumber);
-        await mainPage.typeMonthYearInput(invalidCreditCardData.expirationDate)
-        await mainPage.typeCVCInput(invalidCreditCardData.cvc)
-        await mainPage.clickContinueBtn()
-        await mainPage.typeFirstNameInput(testContactData.firstName)
-        await mainPage.clickLastNameInput()
-        await mainPage.typeLastNameInput(testContactData.lastName)
-        await mainPage.clickEmailInput()
-        await mainPage.typeEmailInput(testContactData.email)
-        await mainPage.clickPayBtn()
-        await mainPage.cardContinueErrorMessageLabelHasText('Your card was declined. Your request was in live mode, but used a known test card.')
+    await test.step("1. Open the main page", async () => {
+      await mainPage.goto();
     });
-})
+    await test.step('2. Click "Give Now" button', async () => {
+      await mainPage.clickGiveNowBtn();
+    });
+    await test.step('3. Сhoose "Monthly"', async () => {
+      await mainPage.clickMonthlyBtn();
+    });
+    await test.step("4. Apply 100 USD", async () => {
+      await mainPage.selectFromCurrencyList("USD");
+      await mainPage.clickPriceInput();
+      await mainPage.typePriceInput("100");
+    });
+    await test.step('5. Click "Donate Monthly"', async () => {
+      await mainPage.clickDonateMonthlyBtn();
+    });
+    await test.step('6. Remove checkbox "Cover transaction costs" if exist', async () => {
+      await mainPage.clickCoverTransactionCostIfActiveCheckBox();
+    });
+    await test.step("7. Choose credit card as a payment method", async () => {
+      await mainPage.clickCreditCardBtn();
+    });
+    await test.step("8. Type credit card data", async () => {
+      await mainPage.typeCardNumberInput(invalidCreditCardData.cardNumber);
+      await mainPage.clickMonthYearInput();
+      await mainPage.typeMonthYearInput(invalidCreditCardData.expirationDate);
+      await mainPage.typeCVCInput(invalidCreditCardData.cvc);
+    });
+    await test.step('9. Click "Continue"', async () => {
+      await mainPage.clickContinueBtn();
+    });
+    await test.step('10. Type "First name"', async () => {
+      await mainPage.typeFirstNameInput(testContactData.firstName);
+    });
+    await test.step('11. Type "Last name"', async () => {
+      await mainPage.clickLastNameInput();
+      await mainPage.typeLastNameInput(testContactData.lastName);
+    });
+    await test.step("12. Type email adress", async () => {
+      await mainPage.clickEmailInput();
+      await mainPage.typeEmailInput(testContactData.email);
+    });
+    await test.step('13. Click "Donate"', async () => {
+      await mainPage.clickPayBtn();
+      await mainPage.cardContinueErrorMessageLabelHasText(
+        invalidCreditCardData.ivalidCardErrorMessage
+      );
+    });
+  });
+
+  test("A2. Payment scenario with incorrect credit card (duplicate)", async ({
+    page,
+  }) => {
+    const mainPage = new MainPage(page);
+
+    await test.step("1. Open the main page", async () => {
+      await mainPage.goto();
+    });
+    await test.step('2. Click "Give Now" button', async () => {
+      await mainPage.clickGiveNowBtn();
+    });
+    await test.step('3. Сhoose "Monthly"', async () => {
+      await mainPage.clickMonthlyBtn();
+    });
+    await test.step("4. Apply 100 USD", async () => {
+      await mainPage.selectFromCurrencyList("USD");
+      await mainPage.clickPriceInput();
+      await mainPage.typePriceInput("100");
+    });
+    await test.step('5. Click "Donate Monthly"', async () => {
+      await mainPage.clickDonateMonthlyBtn();
+    });
+    await test.step('6. Remove checkbox "Cover transaction costs" if exist', async () => {
+      await mainPage.clickCoverTransactionCostIfActiveCheckBox();
+    });
+    await test.step("7. Choose credit card as a payment method", async () => {
+      await mainPage.clickCreditCardBtn();
+    });
+    await test.step("8. Type credit card data", async () => {
+      await mainPage.typeCardNumberInput(invalidCreditCardData.cardNumber);
+      await mainPage.clickMonthYearInput();
+      await mainPage.typeMonthYearInput(invalidCreditCardData.expirationDate);
+      await mainPage.typeCVCInput(invalidCreditCardData.cvc);
+    });
+    await test.step('9. Click "Continue"', async () => {
+      await mainPage.clickContinueBtn();
+    });
+    await test.step('10. Type "First name"', async () => {
+      await mainPage.typeFirstNameInput(testContactData.firstName);
+    });
+    await test.step('11. Type "Last name"', async () => {
+      await mainPage.clickLastNameInput();
+      await mainPage.typeLastNameInput(testContactData.lastName);
+    });
+    await test.step("12. Type email adress", async () => {
+      await mainPage.clickEmailInput();
+      await mainPage.typeEmailInput(testContactData.email);
+    });
+    await test.step('13. Click "Donate"', async () => {
+      await mainPage.clickPayBtn();
+      await mainPage.cardContinueErrorMessageLabelHasText(
+        invalidCreditCardData.ivalidCardErrorMessage
+      );
+    });
+  });
+});
